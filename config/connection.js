770ad20717f9d95ba9d088b586b252097cf3f8ca
Tony Isidro <tonyi@Tonys-MBP-3.fios-router.home>
2018-05-28 16:51:28 -0400

    Added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -8,16 +8,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-if(process.env.JAWSDB_URL){
-  connection =  mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
-  });
-};
+
 
 connection.connect(function(err) {
   if (err) {
